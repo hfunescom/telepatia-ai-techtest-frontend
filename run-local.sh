e9fa5cf9dc41f2c7711e1e0177c320099efcb1a7
Hernán Javier Funes <hernan.funes@gmail.com>
2025-09-04 22:36:59 -0300

    feat: run-local.sh moved

---
+++ run-local.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd telepatia_ai_techtest_frontend/
+#download libraries
+flutter pub get
+#run frontend
+flutter run -d chrome
--- telepatia_ai_techtest_frontend/run-local.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-flutter pub get
-flutter run -d chrome
